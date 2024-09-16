142be0a9ac080d6028ca510c74bcae81b08ecd44
alyssa <ahuggins4@my.bcit.ca>
2024-09-16 11:09:30 -0700

    review

--- Test.js
+++ Test.js
@@ -11,3 +11,7 @@ sewtype = "quilting";
 
 console.log("\x1b[0m","My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+2+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" space I use and a space for storing my fabrics.")
 console.log("\x1b[34m","=========================","\x1b[0m")
+
+//reviewed by Thea
+//Adjusted number type to simplify
+//helped add color
