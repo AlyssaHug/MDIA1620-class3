b00cb3809686846ac6187266502ccf8f92f828d6
alyssa <ahuggins4@my.bcit.ca>
2024-09-16 10:47:31 -0700

    color

--- Test.js
+++ Test.js
@@ -8,5 +8,5 @@ console.log("\x1b[34m","========================")
 
 sewtype = "quilting" 
 
-console.log("\x1b[0m","My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" I use and a space for storing my fabrics.")
+console.log("\x1b[0m","My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" space I use and a space for storing my fabrics.")
 console.log("\x1b[34m","=========================","\x1b[0m")
