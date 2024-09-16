1ce25edd406056b2780d76ca2d7ee6dc69bd8688
alyssa <ahuggins4@my.bcit.ca>
2024-09-16 10:43:55 -0700

    color

--- Test.js
+++ Test.js
@@ -1,12 +1,12 @@
 const hobby = "sewing";
 let sewtype = "machine sewing";
 let number = "2"
-console.log("========================")
+console.log("\x1b[34m","========================")
 
-console.log("My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" I use and a space for storing my fabrics.")
-console.log("========================")
+console.log("\x1b[0m","My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" I use and a space for storing my fabrics.")
+console.log("\x1b[34m","========================")
 
 sewtype = "quilting" 
 
-console.log("My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" I use and a space for storing my fabrics.")
-console.log("=========================")
+console.log("\x1b[0m","My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" I use and a space for storing my fabrics.")
+console.log("\x1b[34m","=========================","\x1b[0m")
