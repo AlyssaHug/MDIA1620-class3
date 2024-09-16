cc2c4cc0fd0ce8b2366f8ad626ca121e31fbe9d1
alyssa <ahuggins4@my.bcit.ca>
2024-09-16 10:37:23 -0700

    hobby

---
+++ Test.js
@@ -0,0 +1,12 @@
+const hobby = "sewing";
+let sewtype = "machine sewing";
+let number = "2"
+console.log("========================")
+
+console.log("My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" I use and a space for storing my fabrics.")
+console.log("========================")
+
+sewtype = "quilting" 
+
+console.log("My name is Alyssa and I enjoy "+hobby+". "+hobby+" keeps me busy and lets me put my spare time in a creative outlet. I learned different types of "+hobby+" through highschool, but since I mainly sew clothes I stick to "+number+" types, hand "+hobby+" and "+sewtype+". I have a "+sewtype+" I use and a space for storing my fabrics.")
+console.log("=========================")
